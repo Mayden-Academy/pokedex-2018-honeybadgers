578099770bbf77f5db73ffb678455d8c3132a90d
Daniel <hoptond848@protonmail.com>
2018-11-12 14:50:06 +0000

    lay out login page

---
+++ login.php
@@ -0,0 +1,23 @@
+<?php
+
+?>
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <title>Pokédex</title>
+</head>
+<body>
+<header>
+    <h1>POKEDEX</h1>
+</header>
+<main>
+    <form>
+        <label for="email">Email</label>
+        <input id="email" type="email">
+        <input type="submit" value="Login">
+    </form>
+    <p></p>
+</main>
+</body>
+</html>
