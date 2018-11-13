0f7fa14cb4453f449a0df913aa7315023609cca0
Daniel <hoptond848@protonmail.com>
2018-11-13 10:46:06 +0000

    fix Pokemon __get magic method

--- src/Pokemon.php
+++ src/Pokemon.php
@@ -2,6 +2,8 @@
 
 namespace Pokedex;
 
+use Pokedex\Exceptions\PokemonException;
+
 class Pokemon {
 
     private $id;
@@ -11,23 +13,20 @@ class Pokemon {
     public function __construct($id, $name, $types) {
 
         if(!is_int($id)) {
-            throw new \Exception('expected integer in $id parameter, given ' . gettype($id) . ' instead.');
+            throw new PokemonException('expected integer in $id parameter, given ' . gettype($id) . ' instead.');
         }
         $this->id = $id;
         $this->name = $name;
         if (!is_array($types)) {
-            throw new \Exception('expected array in $types parameter, given ' . gettype($types) . ' instead.');
+            throw new PokemonException('expected array in $types parameter, given ' . gettype($types) . ' instead.');
         }
         $this->types = $types;
     }
 
     public function __get($propertyName) {
-        if (property_exists('Pokemon', $propertyName)) {
+        if (property_exists('\\Pokedex\\Pokemon', $propertyName)) {
             return $this->$propertyName;
         }
-        throw new \Exception('Property ' . $propertyName . '  does not exist on the Pokemon class');
+        throw new PokemonException('Property ' . $propertyName . '  does not exist on the Pokemon class');
     }
-
-
-
 }
