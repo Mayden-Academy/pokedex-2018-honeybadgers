b656cb252d539537c5c6c2b4173ed08c9846d05b
Felix <felixgate13@gmail.com>
2018-11-13 11:11:51 +0000

    modified gulpfile to allow for more scss files made import folder and variables.scss created index.php with basic structure

--- index.php
+++ index.php
@@ -1,15 +1,44 @@
 <?php
 
-header('Location: login.php');
 
 ?>
 <!DOCTYPE html>
 <html lang="en">
 <head>
     <meta charset="UTF-8">
+    <link href="https://fonts.googleapis.com/css?family=Cantarell:400,700" rel="stylesheet">
+    <link rel="stylesheet" href="css/normalize.css">
+    <link rel="stylesheet" href="css/index.css">
     <title>Pokédex</title>
 </head>
 <body>
+<main>
+    <form>
+        <div id="scroll">
+            <ul>
+                <li class="poke-container">
+                    <div class="info">
+                        <h2>Name of Pokemon</h2>
+                        <ul>
+                            <li>
+                                <p>type</p>
+                                <p>type</p>
+                            </li>
+                        </ul>
+                    </div>
+                    <div class="picture-holder">
+                        <div class="picture">
+
+
+                        </div>
+                    </div>
+
+                </li>
+            </ul>
+        </div>
+        <input type="submit" placeholder="Save">
+    </form>
+</main>
 
 </body>
 </html>
