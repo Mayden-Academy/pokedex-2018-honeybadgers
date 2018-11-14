e4936d4058b7bc1d96bcf9389bb4b5c47d23f67e
Burak Tilek <burak.tilek@gmail.com>
2018-11-14 13:45:31 +0000

    Removed unnecassary div wrapping ul, #scrolls height changed into 80vh

--- index.php
+++ index.php
@@ -10,47 +10,45 @@
     <title>Pokédex</title>
 </head>
 <body>
-<main>
-    <form>
-        <div id="scroll">
-            <ul>
-                <li class="poke-container">
-                    <div class="info">
-                        <div class="upper">
-                            <h2 class="poke-title">Pikachu</h2>
-                            <h2 class="poke-id">#25</h2>
-                        </div>
-                        <div class="lower">
-                            <div class="type-holder">
-                                <p class="type">type</p>
-                                <p class="type">type</p>
+    <main>
+        <form>
+                <ul id="scroll">
+                    <li class="poke-container">
+                        <div class="info">
+                            <div class="upper">
+                                <h2 class="poke-title">Pikachu</h2>
+                                <h2 class="poke-id">#25</h2>
                             </div>
-                                <div class="poke-attributes">
-                                    <div class="icon-container">
-                                        <img src="img/Eye_closed.svg" class="icon">
-                                        <p>0</p>
-                                    </div>
-                                    <div class="icon-container">
-                                        <img src="img/Eye_opened.svg" class="icon">
-                                        <p>0</p>
-                                    </div>
-                                    <div class="icon-container">
-                                        <img src="img/pokeball-2.svg" class="icon">
-                                        <p>0</p>
-                                    </div>
+                            <div class="lower">
+                                <div class="type-holder">
+                                    <p class="type">type</p>
+                                    <p class="type">type</p>
                                 </div>
+                                    <div class="poke-attributes">
+                                        <div class="icon-container">
+                                            <img src="img/Eye_closed.svg" class="icon">
+                                            <p>0</p>
+                                        </div>
+                                        <div class="icon-container">
+                                            <img src="img/Eye_opened.svg" class="icon">
+                                            <p>0</p>
+                                        </div>
+                                        <div class="icon-container">
+                                            <img src="img/pokeball-2.svg" class="icon">
+                                            <p>0</p>
+                                        </div>
+                                    </div>
+                            </div>
                         </div>
-                    </div>
-                    <div class="picture-holder">
-                            <img src="https://pokeres.bastionbot.org/images/pokemon/25.png" class="picture">
-                    </div>
-                </li>
-            </ul>
-        </div>
-        <footer>
-            <input type="submit" value="Save" id="save">
-        </footer>
-    </form>
-</main>
+                        <div class="picture-holder">
+                                <img src="https://pokeres.bastionbot.org/images/pokemon/25.png" class="picture">
+                        </div>
+                    </li>
+                </ul>
+            <footer>
+                <input type="submit" value="Save" id="save">
+            </footer>
+        </form>
+    </main>
 </body>
 </html>
