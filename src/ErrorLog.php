145a07c31ee17c8ef54e077de4d94a69ccc9f69a
Patrick <patrick.kenyon@gmail.com>
2018-11-14 11:59:55 +0000

    added whitespace to errorLog.php log function

--- src/ErrorLog.php
+++ src/ErrorLog.php
@@ -9,7 +9,8 @@ class ErrorLog
      * This logs messages to the error log
      * @param string $error Message can be user defined or accessed from Exception object
      */
-    public static function log(string $errorMessage) {
+    public static function log(string $errorMessage)
+    {
         $errorLog = fopen(self::ERROR_LOG, 'a+');
         fwrite($errorLog, $errorMessage . " " . date("Y/m/d H:i:s") . "\n");
         fclose($errorLog);
