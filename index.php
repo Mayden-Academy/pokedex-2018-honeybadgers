479b4e155d1da0441df699ce0fae75c6447983ba
Burak Tilek <burak.tilek@gmail.com>
2018-11-14 14:01:17 +0000

    Indentation fixed

--- index.php
+++ index.php
@@ -12,39 +12,39 @@
 <body>
     <main>
         <form>
-                <ul id="scroll">
-                    <li class="poke-container">
-                        <div class="info">
-                            <div class="upper">
-                                <h2 class="poke-title">Pikachu</h2>
-                                <h2 class="poke-id">#25</h2>
+            <ul id="scroll">
+                <li class="poke-container">
+                    <div class="info">
+                        <div class="upper">
+                            <h2 class="poke-title">Pikachu</h2>
+                            <h2 class="poke-id">#25</h2>
+                        </div>
+                        <div class="lower">
+                            <div class="type-holder">
+                                <p class="type">type</p>
+                                <p class="type">type</p>
                             </div>
-                            <div class="lower">
-                                <div class="type-holder">
-                                    <p class="type">type</p>
-                                    <p class="type">type</p>
+                            <div class="poke-attributes">
+                                <div class="icon-container">
+                                    <img src="img/Eye_closed.svg" class="icon">
+                                    <input type="radio" name="status">
                                 </div>
-                                    <div class="poke-attributes">
-                                        <div class="icon-container">
-                                            <img src="img/Eye_closed.svg" class="icon">
-                                            <p>0</p>
-                                        </div>
-                                        <div class="icon-container">
-                                            <img src="img/Eye_opened.svg" class="icon">
-                                            <p>0</p>
-                                        </div>
-                                        <div class="icon-container">
-                                            <img src="img/pokeball-2.svg" class="icon">
-                                            <p>0</p>
-                                        </div>
-                                    </div>
+                                <div class="icon-container">
+                                    <img src="img/Eye_opened.svg" class="icon">
+                                    <input type="radio" name="status">
+                                </div>
+                                <div class="icon-container">
+                                    <img src="img/pokeball-2.svg" class="icon">
+                                    <input type="radio" name="status">
+                                </div>
                             </div>
                         </div>
-                        <div class="picture-holder">
-                                <img src="https://pokeres.bastionbot.org/images/pokemon/25.png" class="picture">
-                        </div>
-                    </li>
-                </ul>
+                    </div>
+                    <div class="picture-holder">
+                            <img src="https://pokeres.bastionbot.org/images/pokemon/25.png" class="picture">
+                    </div>
+                </li>
+            </ul>
             <footer>
                 <input type="submit" value="Save" id="save">
             </footer>
