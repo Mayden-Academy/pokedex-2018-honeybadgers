5e9e397261dc77095161c88e6d039392935cae86
Daniel <hoptond848@protonmail.com>
2018-11-14 14:26:13 +0000

    refactor pokemon and pokelist classes

--- src/PokeList.php
+++ src/PokeList.php
@@ -12,21 +12,22 @@ use Pokedex\DbConnection;
 
 class PokeList {
 
-    private $pokelist;
+    private $list;
 
+    /**
+     * Creates a new pokeList using a DBConnection
+     * @param \Pokedex\DbConnection $connection The particular connection to use for retrieving the list of pokemon.
+     */
     public function __construct(DbConnection $connection) {
         $stmt = $connection->getDB()->prepare('SELECT `id`, `name`, `type_1`, `type_2` FROM `pokemon`');
         $stmt->execute();
-        $pokeresults = $stmt->fetchAll();
-        foreach ($pokeresults as $pokeentry) {
-            $this->pokelist[] = new \Pokedex\Pokemon($pokeentry['id'], $pokeentry['name'], [$pokeentry['type_1'], $pokeentry['type_2']]);
-        }
+        $this->list = $stmt->fetchAll(\PDO::FETCH_CLASS, 'Pokedex\Pokemon');
     }
 
     /**
      * @return array Returns an array containing the pokemon.
      */
     public function getPokemon() : array {
-        return $this->pokelist;
+        return $this->list;
     }
 }
--- src/Pokemon.php
+++ src/Pokemon.php
@@ -2,41 +2,42 @@
 
 namespace Pokedex;
 
-use Pokedex\Exceptions\PokemonException;
-
 class Pokemon {
 
     private $id;
     private $name;
-    private $types;
+    private $type_1;
+    private $type_2;
 
     /**
-     * This constructs a single pokemon instance and sets the properties accordingly.
-     *
-     * @param int $id The pokemon's id.
-     * @param string $name The pokemon's name.
-     * @param array $types $types The pokemon's types.
+     * @return int The pokemon's ID.
      */
-    public function __construct(int $id, string $name, array $types) {
-        $this->id = $id;
-        $this->name = $name;
-        $this->types = $types;
-        if ($this->types[1] === NULL) {
-            array_pop($this->types);
-        }
+    public function getId()
+    {
+        return $this->id;
     }
 
     /**
-     * Returns any property if it exists.  Otherwise, throws a PokemonException
-     *
-     * @param string $propertyName The property the caller is trying to access.
-     * @return mixed the value belonging to the given property.
-     * @throws PokemonException if the property does not exist on the class.
+     * @return string The pokemon's name.
      */
-    public function __get($propertyName) {
-        if (property_exists('\\Pokedex\\Pokemon', $propertyName)) {
-            return $this->$propertyName;
-        }
-        throw new PokemonException('Property ' . $propertyName . '  does not exist on the Pokemon class');
+    public function getName()
+    {
+        return $this->name;
+    }
+
+    /**
+     * @return string The pokemon's first type.
+     */
+    public function getType1()
+    {
+        return $this->type_1;
+    }
+
+    /**
+     * @return mixed The pokemon's second type.
+     */
+    public function getType2()
+    {
+        return $this->type_2;
     }
 }
