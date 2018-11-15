e54fffe9000250cc4604ef1d0c2394890efc50b1
Daniel <hoptond848@protonmail.com>
2018-11-15 11:46:47 +0000

    add PokeUpdateException class

---
+++ src/Exceptions/PokeUpdateException.php
@@ -0,0 +1,14 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: academy
+ * Date: 15/11/2018
+ * Time: 11:40
+ */
+
+namespace Pokedex\Exceptions;
+
+class PokeUpdateException extends \Exception
+{
+
+}
