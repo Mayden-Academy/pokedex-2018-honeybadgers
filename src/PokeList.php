ea53e7e51592732f40dcf0f20cf9d33b986ce9b0
Patrick <patrick.kenyon@gmail.com>
2018-11-15 14:22:08 +0000

    modified sql to order pokemon by pokemon id in PokeList.php

--- src/PokeList.php
+++ src/PokeList.php
@@ -1,11 +1,4 @@
 <?php
-/**
- * Created by PhpStorm.
- * User: academy
- * Date: 14/11/2018
- * Time: 11:48
- */
-
 namespace Pokedex;
 
 class PokeList {
@@ -13,19 +6,19 @@ class PokeList {
     private $pokemon;
 
     /**
-     * Creates a new pokeList using a DBConnection
+     * Creates a new pokeList using a PDO object
      * @param \PDO $connection The particular database object to use for retrieving the list of pokemon.
      */
     public function __construct(\PDO $db, int $userId)
     {
-        //$stmt = $db->prepare('SELECT `id`, `name`, `type_1`, `type_2` FROM `pokemon`');
         $stmt = $db->prepare('
           SELECT `pokemon`.`id`, `pokemon`.`name`, `pokemon`.`type_1`, `pokemon`.`type_2`, `new_status_table`.`seen_caught` 
           FROM `pokemon` 
           LEFT JOIN (SELECT `pokemon_id`, `seen_caught` 
           FROM `status` 
           WHERE `user_id` = ?) 
-          AS new_status_table ON `pokemon`.`id` = `new_status_table`.`pokemon_id`;');
+          AS new_status_table ON `pokemon`.`id` = `new_status_table`.`pokemon_id`
+          ORDER BY `pokemon`.`id`;');
         $stmt->execute([$userId]);
         $this->pokemon = $stmt->fetchAll(\PDO::FETCH_CLASS, 'Pokedex\Pokemon');
     }
